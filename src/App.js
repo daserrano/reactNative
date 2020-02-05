cded92743313656de0a82e42c909d9dc1b5157c1
daserrano <david.serrano@samelan.com>
2020-02-05 14:05:17 +0100

    first

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+// function Hello (props) {
+//   return <h3>{props.title}</h3>
+// }
+
+// const Hello = (props) => <h3>{props.title}</h3>
+
+class Hello extends Component {
+  render() {
+    return <h3>{this.props.title}</h3>
+  }
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>iAhorro</h2>
+        <Hello title='Buscar campaña (from props)'/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
