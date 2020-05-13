564dfcc62b92120083acf938e322cdd8ddcd4584
daserrano <sein2809@gmail.com>
2020-05-13 13:35:36 +0200

    Primer evento

--- src/App.js
+++ src/App.js
@@ -1,35 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import cars from './data/cars.json'
-
-class CarItem extends Component {
-  render() {
-    const {car} = this.props
-
-    return (
-      <li key={car.id}>
-        <p><strong>Marca: </strong>{car.company}</p>
-        <p><strong>Nombre: </strong>{car.name}</p>
-        <p><strong>Motor: </strong>{car.engine}</p>
-      </li>
-    )
-  }
-}
 
 class App extends Component {
     render() {
     return (
     <div className="App">
       <header className="App-header">
-        <h4>Trabajando con listas con objetos</h4>
-        <ul>
-          {
-            cars.map(car => {
-              return <CarItem key={car.id} car={car}/>
-            })
-          }
-        </ul>
+        <h4>Eventos</h4>
+        <button onClick={() => alert('EVENTO')}> Llamar evento </button>
       </header>
     </div>
   );
