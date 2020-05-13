8db9173fc697cffe6be0938d481090614e0410dc
daserrano <sein2809@gmail.com>
2020-05-13 19:33:44 +0200

    Formulario

--- src/App.js
+++ src/App.js
@@ -3,39 +3,36 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {mouseX: 0, mouseY: 0}
-    //Forma de enlazar el contexto.
-    //this.handleMouseMove = this.handleMouseMove.bind(this)
-  }
-  //Arrow function para enlazar el contexto del componente, forma óptima.
-  handleMouseMove = (e) => {
-    const {clientX, clientY} = e
-      this.setState({mouseX: clientX, mouseY: clientY })
-  }
-
   handleClick(e) {
-    console.log(e)
-    alert('Evento!')
+    e.preventDefault()
+    const name = document.getElementById('name').value
+    const email = document.getElementById('twitter').value
+    console.log({name, email});
   }
-
-    render() {
+  render() {
     return (
     <div className="App">
       <header className="App-header">
-        <h4>Eventos</h4>
-        <button onClick={this.handleClick}> Llamar evento </button>
-        <div onMouseMove={this.handleMouseMove}
-        style={{
-          border: '1px solid #FFF',
-          marginTop: 10,
-          padding: 10,
-          paddingLeft: 100,
-          paddingRight:100}
-        }>
-          <p>{this.state.mouseX}, {this.state.mouseY}</p>
-        </div>
+        <h4>Formularios</h4>
+        <form>
+          <p>
+            <label>Nombre: </label>
+            <input
+              id='name'
+              name='userName'
+              placeholder='Introduce tu nombre'/>
+          </p>
+
+          <p>
+            <label>Twitter: </label>
+            <input
+              id='twitter'
+              name='twitterAccount'
+              placeholder='Introduce tu Twitter' />
+          </p>
+
+          <button onClick={this.handleClick}>Enviar</button>
+        </form>
       </header>
     </div>
   );
