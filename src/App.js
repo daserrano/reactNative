a15a60f12223537f7560b4d5ec3076b379f929b7
daserrano <sein2809@gmail.com>
2020-05-13 14:06:21 +0200

    Eventos onClick y onMouseMove. Bind de contexto de componente.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,39 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {mouseX: 0, mouseY: 0}
+    //Forma de enlazar el contexto.
+    //this.handleMouseMove = this.handleMouseMove.bind(this)
+  }
+  //Arrow function para enlazar el contexto del componente, forma óptima.
+  handleMouseMove = (e) => {
+    const {clientX, clientY} = e
+      this.setState({mouseX: clientX, mouseY: clientY })
+  }
+
+  handleClick(e) {
+    console.log(e)
+    alert('Evento!')
+  }
+
     render() {
     return (
     <div className="App">
       <header className="App-header">
         <h4>Eventos</h4>
-        <button onClick={() => alert('EVENTO')}> Llamar evento </button>
+        <button onClick={this.handleClick}> Llamar evento </button>
+        <div onMouseMove={this.handleMouseMove}
+        style={{
+          border: '1px solid #FFF',
+          marginTop: 10,
+          padding: 10,
+          paddingLeft: 100,
+          paddingRight:100}
+        }>
+          <p>{this.state.mouseX}, {this.state.mouseY}</p>
+        </div>
       </header>
     </div>
   );
