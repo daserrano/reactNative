336081fa3104dcbd26a86b658516dac46ef3f8da
daserrano <sein2809@gmail.com>
2020-05-14 13:05:29 +0200

    Children props.

--- src/App.js
+++ src/App.js
@@ -2,63 +2,24 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Box extends Component {
+  render() {
+    return (
+      <div style={{ border: '1px solid #FFF', margin: 5, padding: 5 }}>
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
 class App extends Component {
-
-  constructor() {
-    super()
-    this.state = {
-      inputName: '',
-      inputTwitter: '@',
-      inputTerms: true
-    }
-  }
-  handleSubmit = (e) => {
-    e.preventDefault()
-    console.log(this.state);
-  }
-
-  handleChange = (e) => {
-    console.log('handleChange')
-    console.log(e.target.checked)
-    this.setState({inputTerms: e.target.checked})
-  }
   render() {
     return (
     <div className="App">
       <header className="App-header">
-        <h4>Formularios</h4>
-        <form onSubmit={this.handleSubmit}>
-          <p>
-            <label htmlFor='name'>Nombre: </label>
-            <input
-              id='name'
-              name='userName'
-              placeholder='Introduce tu nombre'
-              value={this.state.inputName}
-              onChange={e => this.setState({inputName: e.target.value})}/>
-          </p>
-
-          <p>
-            <label>Twitter: </label>
-            <input
-              id='twitter'
-              name='twitterAccount'
-              placeholder='Introduce tu Twitter'
-              value={this.state.inputTwitter}
-              onChange={e => this.setState({inputTwitter: e.target.value})}/>
-          </p>
-
-          <p>
-            <label>
-              <input
-              type='checkbox'
-              onChange={this.handleChange}
-              checked={this.state.inputTerms}/>
-              Accepted terms
-            </label>
-          </p>
-          <button>Enviar</button>
-        </form>
+        <h4>Children props</h4>
+        <Box>Hola mundo!</Box>
+        <Box>Otro box</Box>
       </header>
     </div>
   );
