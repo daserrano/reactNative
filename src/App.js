892f1f626f1a9ffba9cdaff758b68906c47047ab
daserrano <sein2809@gmail.com>
2020-05-14 20:07:42 +0200

    Ciclo de vida>montaje>constructor

--- src/App.js
+++ src/App.js
@@ -1,52 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import PropTypes from 'prop-types'
-
-class Box extends Component {
-  render() {
-    return (
-      <div style={{ border: '1px solid #FFF', margin: 5, padding: 5 }}>
-        {this.props.children}
-      </div>
-    )
-  }
-}
-
-class Article extends Component {
-  static propTypes =  {
-    author: PropTypes.string.isRequired
-  }
-  render() {
-    const { author, children, date, title} = this.props
-    return (
-      <section>
-        <h2>{title}</h2>
-        {author && <p><em>Escrito por {author}</em></p>}
-        <Box>{date}</Box>
-        <article>
-          {children}
-        </article>
-      </section>
-    )
-  }
-}
 
 class App extends Component {
+
+  //constructor() {}  //Constructor por defecto.
+
+  constructor(props) {
+    super(props) //Constructor de Component
+    //Inicializamos el state del componente
+    this.state = {mensajeInicial: 'Mensaje inicial'}
+    //bindeamos el contexto al método
+    //this.handleClick = this.handleClick.bind(this)
+  }
+
+  //Otra forma de tener el contexto es el arrow function
+  handleClick = () => {
+    this.setState({mensajeInicial: 'Mensaje cambiado'})
+  }
+
   render() {
     return (
     <div className="App">
       <header className="App-header">
-        <h4>Children props</h4>
-        <Article
-          date={new Date().toLocaleDateString()}
-          title='Artículo sobre la prop children'
-        >
-          <p>El contenido que envolvemos dentro del componente Article será
-          enviado al componente como this.props.children</p>
-          <strong>Y mantiene las etiquetas y componentes que haya
-          añadido dentro</strong>
-        </Article>
+        <h4>Ciclo de montaje: constructor</h4>
+        {this.state.mensajeInicial}
+        <button onClick={this.handleClick}>
+          Cambiar mensaje
+        </button>
       </header>
     </div>
   );
