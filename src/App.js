223595edaf32034147b3b7b09d235e0c4a78e2b0
daserrano <sein2809@gmail.com>
2020-05-14 14:14:01 +0200

    Añadida dependencia PropTypes. Prop con tipo y obligatorio. Solamente se utiliza en desarrollo.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PropTypes from 'prop-types'
 
 class Box extends Component {
   render() {
@@ -12,14 +13,40 @@ class Box extends Component {
   }
 }
 
+class Article extends Component {
+  static propTypes =  {
+    author: PropTypes.string.isRequired
+  }
+  render() {
+    const { author, children, date, title} = this.props
+    return (
+      <section>
+        <h2>{title}</h2>
+        {author && <p><em>Escrito por {author}</em></p>}
+        <Box>{date}</Box>
+        <article>
+          {children}
+        </article>
+      </section>
+    )
+  }
+}
+
 class App extends Component {
   render() {
     return (
     <div className="App">
       <header className="App-header">
         <h4>Children props</h4>
-        <Box>Hola mundo!</Box>
-        <Box>Otro box</Box>
+        <Article
+          date={new Date().toLocaleDateString()}
+          title='Artículo sobre la prop children'
+        >
+          <p>El contenido que envolvemos dentro del componente Article será
+          enviado al componente como this.props.children</p>
+          <strong>Y mantiene las etiquetas y componentes que haya
+          añadido dentro</strong>
+        </Article>
       </header>
     </div>
   );
