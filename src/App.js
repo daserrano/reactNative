051c1229681deb13c24c498ca5c90f43dc0fb158
daserrano <sein2809@gmail.com>
2020-05-14 20:42:54 +0200

    Ciclo de vida>montaje>render

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import './App.css';
 
 class App extends Component {
 
-  //constructor() {}  //Constructor por defecto.
-
   constructor(props) {
     console.log('constructor')
     super(props)
@@ -14,7 +12,6 @@ class App extends Component {
 
   componentWillMount() {
     console.log('componentWillMount')
-    this.setState({mensaje: 'mensaje modificado'})
   }
 
   render() {
@@ -22,8 +19,7 @@ class App extends Component {
     return (
     <div className="App">
       <header className="App-header">
-        <h4>Ciclo de montaje: componentWillMount</h4>
-        {this.state.mensaje}
+        <h4>Ciclo de montaje: render</h4>
       </header>
     </div>
   );
